b6747b8b41c89ade3d645bbe3bbd9bf00ae695ac
Expedito <irospidermanstronger@gmail.com>
2024-06-08 18:52:49 -0300

    Adicionados Programa maior e menor entre três Maior entre três

---
+++ exercicios/src/EX06_MaiorEntreTres.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class EX06_MaiorEntreTres {
+
+    public static void Maior() {
+        Scanner sc = new Scanner(System.in);
+        int tentativas = 3;
+        int maior = Integer.MIN_VALUE;
+        for (int i = 0; i < tentativas; i++) {
+            System.out.print("Digite um número: ");
+            int num = sc.nextInt();
+            int numeros = num;
+
+            if (numeros > maior) {
+                maior = numeros;
+            }
+        }
+        System.out.printf("O  maior entre os três é %d", maior);
+    }
+
+    public static void main(String[] args) {
+        Maior();
+    }
+
+}
