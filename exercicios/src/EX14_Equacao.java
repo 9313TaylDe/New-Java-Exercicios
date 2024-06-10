9cfb4805dd775ad9228ccde42a0a93e21c16f184
Expedito <irospidermanstronger@gmail.com>
2024-06-10 17:20:45 -0300

    Programa equação

--- exercicios/src/EX14_Equacao.java
+++ exercicios/src/EX14_Equacao.java
@@ -8,24 +8,35 @@ public class EX14_Equacao {
 
         System.out.print("Digite a equação");
         System.out.print("\nDigite o valor de A: ");
-        int A = sc.nextInt();
+        int a = sc.nextInt();
         boolean passe = true;
         int x = 0;
+        int c = 0;
+        int b = 0;
         double equacao = 0;
 
-        if (A == 0) {
+        if (a == 0) {
             System.out.print("Programa encerrado!");
             System.out.println("\nNão é equação de segundo grau");
         }
         passe = false;
 
-        if (A != 0) {
+        if (a != 0) {
             System.out.print("Digite o valor de B: ");
-            int B = sc.nextInt();
+            b = sc.nextInt();
             System.out.print("Digite o valor de C: ");
-            int C = sc.nextInt();
+            c = sc.nextInt();
 
-            equacao = (A * (Math.pow(x, 2) + B * x) + C);
+            equacao = (a * (Math.pow(x, 2) + b * x) + c);
+        }
+
+        double delta = ((Math.pow(b, 2)) )- (4 * (a * c));
+        if (delta == 0) {
+            System.out.print("\nA equação não possui raizes reais.");
+        } else if (delta < 0) {
+            System.out.print("\nA equação possui apenas uma raiz real.");
+        } else if (delta > 0) {
+            System.out.print("\nA equação possui duas raizes reais.");
         }
         System.out.print("\nA equação é: " + equacao);
     }
