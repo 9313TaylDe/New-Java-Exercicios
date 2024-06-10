25d0ed4401bf6287c74e1395893a5ffcf8875b55
Expedito <irospidermanstronger@gmail.com>
2024-06-10 18:04:32 -0300

    Programa ano bissexto

---
+++ exercicios/src/EX15_Ano_Bissexto.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class EX15_Ano_Bissexto {
+
+    public static void Bissexto() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite um ano: ");
+        int ano = sc.nextInt();
+
+        if (ano <= 0) {
+            System.out.print("Ano inválido");
+        }
+        else if(ano % 2 == 0 || ano % 400 == 0) {
+            System.out.printf("Ano é bissexto: %d", ano);
+        }else if(ano % 2 != 0 || ano %  400 != 0){
+            System.out.printf("Ano não é bissexto: %d", ano);
+        }
+    }
+
+    public static void main(String[] args) {
+        Bissexto();
+    }
+
+}
