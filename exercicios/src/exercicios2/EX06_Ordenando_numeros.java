5d2c817e865ae63044d9d6369387fa8d69d0ffb7
Expedito <irospidermanstronger@gmail.com>
2024-06-14 15:23:57 -0300

    Programa odernando números.

---
+++ exercicios/src/exercicios2/EX06_Ordenando_numeros.java
@@ -0,0 +1,16 @@
+package exercicios2;
+
+public class EX06_Ordenando_numeros {
+    public static void main(String[] args) {
+        int i = 0;
+        for (i = 20; i >= 1; --i) {
+            System.out.println(i);
+        }
+        System.out
+                .println(
+                        "\n-----------------------------------------------------------------------------------------------\n");
+        for (i = 20; i >= 1; --i) {
+            System.out.printf("%d - ", i);
+        }
+    }
+}
