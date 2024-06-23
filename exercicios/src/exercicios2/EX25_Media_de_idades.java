3c1dcc4cfdc50226de31f15019dfc181e04fd48e
Expedito <irospidermanstronger@gmail.com>
2024-06-23 15:01:10 -0300

    Programa media das idades

--- exercicios/src/exercicios2/EX25_Media_de_idades.java
+++ exercicios/src/exercicios2/EX25_Media_de_idades.java
@@ -15,6 +15,11 @@ public class EX25_Media_de_idades {
             idade[i] =sc.nextDouble();
             media += idade[i] / 5;
         }
+        if(media > 0 || media <= 25){
+            System.out.printf("A média %.1f está entre 0 e 25. ", media);
+        }else if(media >= 26 || media <= 60){
+            System.out.printf("A média %.1f estáentre 26 e 60",media);
+        }
         System.out.printf("A média de idade será de %.1f", media);
     }
     public static void main(String[] args) {
