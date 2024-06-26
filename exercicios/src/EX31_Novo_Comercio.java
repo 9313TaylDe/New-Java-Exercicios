0d532ac1b533c29dc34cfbedb056d82af078d8bc
expedito <expeditotaylor@gmail.com>
2024-06-26 17:21:58 -0300

    Novo programa

---
+++ exercicios/src/EX31_Novo_Comercio.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class EX31_Novo_Comercio {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double novo_valor = 0;
+        double total = 0;
+        int opcao = 1;
+        double[] valor = new double[30];
+        double index = 0;
+        int i = 0;
+        while (opcao != 0) {
+            System.out.print("Somar? ");
+            opcao = sc.nextInt();
+            if (opcao == 0) {
+                System.out.printf("Valor total: R$%.2f", total);
+                System.out.println("\nPrograma encerrado");
+                break;
+            } else {
+                for (i = 1; i < 6; i++) {
+                    System.out.print("Digite o valor: ");
+                    valor[i] = sc.nextDouble();
+
+                    novo_valor += valor[i];
+                    total = novo_valor;
+                    index = valor[i];
+                }
+                for (i = 1; i < 6; i++) {
+
+                    System.out.printf("\n%d - R$%.2f", i, valor[i]);
+                }
+                System.out.printf("\nValor total: R$%.2f", total);
+                System.out.println("\nPROGRAMA ENCERRADO");
+            }
+        }
+    }
+}
